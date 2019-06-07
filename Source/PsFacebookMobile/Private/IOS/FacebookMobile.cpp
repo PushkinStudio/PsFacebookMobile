4b4c6363c21813b6b235610bb8ab4d8042cba0fa
Vladimir Alyamkin <ufna@ufna.ru>
2019-06-07 13:19:33 +0300

    Add observer declaration #5

--- Source/PsFacebookMobile/Private/IOS/FacebookMobile.cpp
+++ Source/PsFacebookMobile/Private/IOS/FacebookMobile.cpp
@@ -1,5 +1,7 @@
 // Copyright 2015-2019 Mail.Ru Group. All Rights Reserved.
 
+#include "FacebookMobile.h"
+
 #if PLATFORM_IOS
 
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
---
+++ Source/PsFacebookMobile/Private/IOS/FacebookMobile.h
@@ -0,0 +1,6 @@
+// Copyright 2015-2019 Mail.Ru Group. All Rights Reserved.
+
+#if PLATFORM_IOS
+@interface FacebookObserver : NSObject
+@end
+#endif // PLATFORM_IOS
