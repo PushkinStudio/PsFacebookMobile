9d5db1aa800bf645c3cb2a6057230a95afb49e74
Anton Rassadin <toha90@gmail.com>
2019-11-20 20:33:02 +0300

    Add didFinishLaunchingWithOptions registration

--- Source/PsFacebookMobile/Private/IOS/FacebookMobile.h
+++ Source/PsFacebookMobile/Private/IOS/FacebookMobile.h
@@ -1,6 +1,10 @@
 // Copyright 2015-2019 Mail.Ru Group. All Rights Reserved.
 
+#pragma once
+
 #if PLATFORM_IOS
 @interface FacebookObserver : NSObject
++ (FacebookObserver *)sharedInstance;
+- (void)didFinishLaunching:(NSNotification *)notification;
 @end
 #endif // PLATFORM_IOS
