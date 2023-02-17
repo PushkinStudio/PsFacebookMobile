a9a85675d487fe8fec1b96bd8c6357a0a43ac62b
Anton Rassadin <anton@rassadin.net>
2023-02-17 14:59:20 +0300

    Add client token to settings

--- Source/PsFacebookMobile/Public/PsFacebookMobileSettings.h
+++ Source/PsFacebookMobile/Public/PsFacebookMobileSettings.h
@@ -10,15 +10,19 @@ class PSFACEBOOKMOBILE_API UPsFacebookMobileSettings : public UObject
 	GENERATED_UCLASS_BODY()
 
 public:
-	/** */
+	/** Plugin enabled flag */
 	UPROPERTY(Config, EditAnywhere, Category = "Pushkin Facebook Mobile Settings")
 	bool bEnableFacebook;
 
-	/** */
+	/** Facebook App ID */
 	UPROPERTY(Config, EditAnywhere, Category = "Pushkin Facebook Mobile Settings")
 	FString FacebookAppId;
 
-	/** */
+	/** Facebook display name */
 	UPROPERTY(Config, EditAnywhere, Category = "Pushkin Facebook Mobile Settings")
 	FString FacebookDisplayName;
+
+	/** Facebook client token */
+	UPROPERTY(Config, EditAnywhere, Category = "Pushkin Facebook Mobile Settings")
+	FString FacebookClientToken;
 };
